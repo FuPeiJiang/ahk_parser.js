717db0fd295208b50b3b89016b6030703dba5ad7
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2021-05-13 08:23:33 -0400

    arrays.ahk works because findExpression() will skip whiteSpaces

--- src/reconstruct.ts
+++ src/reconstruct.ts
@@ -4,9 +4,9 @@ const d = console.debug.bind(console)
 
 
 const content: string =
-fs.readFileSync('tests2/function definition fix.ahk')
+// fs.readFileSync('tests2/function definition fix.ahk')
 // fs.readFileSync('tests2/class.ahk')
-// fs.readFileSync('tests/arrays.ahk')
+fs.readFileSync('tests/arrays.ahk')
 // fs.readFileSync('tests2/idkAnymore5.ahk')
 // fs.readFileSync('tests/literal objects.ahk')
 // fs.readFileSync('tests2/loop idk.ahk')
