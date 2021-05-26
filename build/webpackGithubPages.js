8823f3d54751ab75aca547dbd263033bb9d519ea
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2021-05-25 23:45:48 -0400

    yarn webpackGithubPages: so you can only use script tag..

--- build/webpackGithubPages.js
+++ build/webpackGithubPages.js
@@ -4,22 +4,20 @@
 
 const path = require('path')
 const rootDir = path.dirname(path.resolve(__dirname)) //it doesn't work without this
-
+// omg just read https://webpack.js.org/guides/author-libraries/
 /**@type {import('webpack').Configuration}*/
 const config = {
-  // target: 'web', // target: 'web' is default https://webpack.js.org/concepts/targets/
+  // target: 'node', // I think I need this for default export ?
+  target: 'web', // target: 'web' is default https://webpack.js.org/concepts/targets/
   mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
   entry: `${rootDir}/src/v1Tov2.ts`, // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
   output: {
     // the bundle is stored in the 'lib' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
     path: `${rootDir}/github pages`,
-    filename: 'convertTov2.js',
+    filename: 'v1Tov2.js',
     //https://webpack.js.org/guides/author-libraries/#expose-the-library
-    library: {
-      name: 'ahkParser',
-      type: 'umd',
-    },
+    library: 'v1Tov2',
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
