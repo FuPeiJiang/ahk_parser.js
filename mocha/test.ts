1dcd0e42da0e12d9a07af600d2dd91ec9fc36e96
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2021-06-12 13:31:24 -0400

    test.ts move functions to the bottom

--- mocha/test.ts
+++ mocha/test.ts
@@ -3,25 +3,6 @@ import ahkParser from '../src/parser/index'
 import modifyEverythingToV2 from '../src/modifyEverythingToV2'
 import fs from 'fs'
 
-function toV2(text) {
-  const everything = ahkParser(text.replace(/\r/g,''))
-  const converted = modifyEverythingToV2(everything)
-  return converted
-}
-function doIt(v1,v2) {
-  it(`\`${v1}\`,\`${v2}\``,function() {
-    strictEqual(toV2(v1),v2)
-  })
-}
-function readFileToString(path) {
-  return fs.readFileSync(`${__dirname}/${path}`).toString()
-}
-function doItFiles(path1,path2) {
-  it(`FILE: '${path1}' vs '${path2}'`,function() {
-    strictEqual(toV2(readFileToString(path1)),readFileToString(path2))
-  })
-}
-
 describe('toV2(text)',function() {
   describe('{} -> Map()',function() {
     doIt('v:={}','v:=Map()')
@@ -43,3 +24,22 @@ describe('toV2(text)',function() {
     doItFiles('../v2tests/A_IsUnicode.ahk','correct/A_IsUnicode.ah2')
   })
 })
+
+function toV2(text) {
+  const everything = ahkParser(text.replace(/\r/g,''))
+  const converted = modifyEverythingToV2(everything)
+  return converted
+}
+function doIt(v1,v2) {
+  it(`\`${v1}\`,\`${v2}\``,function() {
+    strictEqual(toV2(v1),v2)
+  })
+}
+function readFileToString(path) {
+  return fs.readFileSync(`${__dirname}/${path}`).toString()
+}
+function doItFiles(path1,path2) {
+  it(`FILE: '${path1}' vs '${path2}'`,function() {
+    strictEqual(toV2(readFileToString(path1)),readFileToString(path2))
+  })
+}
