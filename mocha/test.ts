0a3c708e98052bbbdab4f886592b8fd69547c66b
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2021-06-12 11:43:36 -0400

    ts-mocha unit tests : mocha/test.ts

---
+++ mocha/test.ts
@@ -0,0 +1,29 @@
+import {strictEqual} from 'assert'
+import ahkParser from '../src/parser/index'
+import modifyEverythingToV2 from '../src/modifyEverythingToV2'
+
+function toV2(text) {
+  const everything = ahkParser(text.replace(/\r/g,''))
+  const converted = modifyEverythingToV2(everything)
+  return converted
+}
+function doIt(v1,v2) {
+  it(`\`${v1}\`,\`${v2}\``,function() {
+    strictEqual(toV2(v1),v2)
+  })
+}
+
+describe('toV2(text)',function() {
+  describe('{} -> Map()',function() {
+    doIt('v:={}','v:=Map()')
+    doIt('v:={a:b}','v:=Map("a",b)')
+    doIt('v:={a:{a:{a:b, c:[]}}}','v:=Map("a",Map("a",Map("a",b, "c",[])))')
+  })
+  describe('VarSetCapacity()',function() {
+    doIt('VarSetCapacity(a,b,c)','a:=BufferAlloc(b,c)')
+    doIt('VarSetCapacity(a,VarSetCapacity(a,b,c),c)','a:=BufferAlloc(a:=BufferAlloc(b,c),c)')
+  })
+  // describe('v1 removed #DIRECTIVES',function() {
+  // doIt('#NoEnv\n','')
+  // })
+})
