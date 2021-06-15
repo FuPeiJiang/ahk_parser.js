237b2cf29d4457aebfe072636fb0d4e90bd0cda7
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2021-06-15 17:22:02 -0400

    extract tests.ts : works

--- mmikeww - AHK-v2-script-converter/extract tests.ts
+++ mmikeww - AHK-v2-script-converter/extract tests.ts
@@ -1,18 +1,45 @@
 import fs from 'fs'
 import ahkParser from '../src/parser/index'
 
+const d = console.debug.bind(console)
+
 const content: Buffer =
-  fs.readFileSync('mmikeww - AHK-v2-script-converter/oof tests/illegal in endExprContinuation.ahk')
-fs.readFileSync('mmikeww - AHK-v2-script-converter/ConvertTests.ahk')
+  // fs.readFileSync('mmikeww - AHK-v2-script-converter/oof tests/illegal in endExprContinuation.ahk')
+  fs.readFileSync('mmikeww - AHK-v2-script-converter/ConvertTests.ahk')
 
 const everything = ahkParser(content.toString().replace(/\r/g,''),true)
 
 let i = 0,b: number
 
-// while (true) {
-// if (findNextTypeText(': ternary')) { break }
-// break
-// }
+while (true) {
+  if (findNextTypeText('assignment','input_script')) { break }
+
+  const iBak = i
+  if (findBackType('function DEFINITION name')) {
+    d('no 2')
+    break
+  }
+  const testName = everything[i].text //use func name as test file name
+  i = iBak
+
+  if (findNextType('StringContinuation')) {
+    d('no 1')
+    break
+  }
+  fs.writeFileSync(`${__dirname}/input/${testName}.ahk`,everything[i].text)
+
+  if (findNextTypeText('assignment','expected')) {
+    d('no 3')
+    break
+  }
+  if (findNextType('StringContinuation')) {
+    d('no 4')
+    break
+  }
+  fs.writeFileSync(`${__dirname}/correct/${testName}.ahk`,everything[i].text)
+}
+
+
 
 const arrToJoin = []
 for (let i = 0,len = everything.length; i < len; i++) {
@@ -21,6 +48,35 @@ for (let i = 0,len = everything.length; i < len; i++) {
 const converted = arrToJoin.join('')
 writeSync(converted,'reconstructed.ah2')
 writeSync(arrOrObjToString(everything),'everything.txt')
+
+function findNextType(dType: string) {
+  let next
+  while ((next = everything[++i])) {
+    if (next.type === dType) {
+      return false
+    }
+  }
+  return true
+}
+function findBackType(dType: string) {
+  let back
+  while ((back = everything[--i])) {
+    if (back.type === dType) {
+      return false
+    }
+  }
+  return true
+}
+
+// function findBackTypeText(dType: string,dText: string) {
+// let back
+// while ((back = everything[--i])) {
+// if (back.type === dType && back.text === dText) {
+// return false
+// }
+// }
+// return true
+// }
 
 function findNextTypeText(dType: string,dText: string) {
   let next
