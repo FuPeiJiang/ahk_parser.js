27c0a518f3f0d08b5be415809699dcafea3b5ea8
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2021-07-25 13:12:13 -0400

    SplashyTest gui add.ahk

--- src/convertTov2.ts
+++ src/convertTov2.ts
@@ -4,7 +4,8 @@ import modifyEverythingToV2 from './modifyEverythingToV2'
 
 
 const content: Buffer =
-fs.readFileSync('v2tests/IniRead.ahk')
+fs.readFileSync('tests3/SplashyTest gui add.ahk')
+// fs.readFileSync('v2tests/IniRead.ahk')
 // fs.readFileSync('tests3/new class.ahk')
 // fs.readFileSync('tests3/Incorrect quoting of parameters #6.ahk')
 // fs.readFileSync('tests3/elseTryFinally.ahk')
---
+++ tests3/SplashyTest gui add.ahk
@@ -0,0 +1,2 @@
+; gui, Test: add, text, % spr " h"ySep " w"3*xSep " v" "t_" c "_" r
+v:=spr " h"ySep " w"3*xSep " v" "t_" c "_" r
